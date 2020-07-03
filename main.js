71ece3efa1158554c80378c3cebbdd1aff36bd2e
Dawish Wishub <dawishwishub@gmail.com>
2020-07-03 13:20:49 -0500

    l

---
+++ main.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const {prefix, bot_info} = require('./config.json');
+client.once('ready', () => {
+    console.log(prefix);
+    console.log(bot_info.version);
+});
+
+bot.login(process.env.token);
+
+client.once('ready', () => {
+    console.log('Dn.Bot is online!');
+});
+
+client.on('message', message =>{
+   if(message.content === `${prefix}ching`){
+       message.channel.send('chong');
+   } else if (message.content === `${prefix}youtube`){
+    message.channel.send('https://www.youtube.com/channel/UCihuEZb13hnq2ZR-yKOjE-Q/');
+   } else if (message.content === `${prefix}robux`){
+    message.channel.send('lmao you wish');
+   } 
+    if (message.content === `${prefix}servername`){
+    message.channel.send(message.guild.name);
+   } else if (message.content === `${prefix}me`){
+    message.channel.send(`Username: ${message.author.tag}`);
+    message.channel.send(`Your ID: ${message.author.id}`);
+   }
+
+});
+
+client.on('message', message => {
+    console.log(message.content);
+});
+
+
