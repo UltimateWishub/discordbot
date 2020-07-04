17158201bdc65f95e9e064300a46f45ca7d23ebb
Dawish Wishub <dawishwishub@gmail.com>
2020-07-04 10:56:09 -0500

    l

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const {prefix, bot_info} = require('./config.json');
-client.once('ready', () => {
-    console.log(prefix);
-    console.log(bot_info.version);
-});
 
 client.login(process.env.token);
 
@@ -12,6 +8,30 @@ client.once('ready', () => {
     console.log('Dn.Bot is online!');
     client.user.setActivity("the deacon server", {type: "WATCHING"});
 })
+
+require("./util/eventHandler")(bot)
+
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
 
 client.on('message', message => {
    if(message.content === `${prefix}ching`){
@@ -28,3 +48,17 @@ client.on('message', message => {
     message.channel.send(`Your ID: ${message.author.id}`);
    } 
 });
+
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let prefix = botsettings.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(bot,message,args)
+
+})
