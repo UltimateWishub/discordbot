702c8f6ed2be52b791167b0161bbded9c234b714
Dawish Wishub <dawishwishub@gmail.com>
2020-07-03 20:54:17 -0500

    Create embeds.js

---
+++ embeds.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    const embed = new Discord.MessageEmbed()
+        .setTitle('this is a test embed')
+}
+
+module.exports.config = {
+    name: "hi",
+    description: "example of an embed.",
+    usage: ".embed",
+    accessableby: "Members",
+    aliases: []
+}
