47b4446ce30a499e33d0f1002ce658e816254e7a
Dawish Wishub <dawishwishub@gmail.com>
2020-07-06 13:42:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
+const { prefix} = require('./config.json');
 
 client.on("ready", async () => {
     console.log('Dn.Bot is online!');
