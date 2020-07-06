c19fbf6a2d3110729d7c2ea3f249578f8a8c1aed
Dawish Wishub <dawishwishub@gmail.com>
2020-07-06 11:12:28 -0500

    l

--- events/ready.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js')
-
-module.exports = client => {
-        console.log('Dn.Bot is online!');
-        client.user.setActivity("the deacon server", {type: "WATCHING"});
-}
--- main.js
+++ main.js
@@ -1,43 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const clientsettings = require("./clientsettings.json");
+const config = require("./config.json");
+
+client.on("ready", async () => {
+    console.log('Dn.Bot is online!');
+    client.user.setActivity("the deacon server", {type: "WATCHING"});
+})
 
 client.login(process.env.token);
 
-require("./util/eventhandler")(client)
 
-const fs = require("fs");
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-
-fs.readdir("./commands/", (err, files) => {
-
-    if(err) console.log(err)
-
-    let jsfile = files.filter(f => f.split(".").pop() === "js") 
-    if(jsfile.length <= 0) {
-         return console.log("[LOGS] Couldn't Find Commands!");
-    }
-
-    jsfile.forEach((f, i) => {
-        let pull = require(`./commands/${f}`);
-        client.commands.set(pull.config.name, pull);  
-        pull.config.aliases.forEach(alias => {
-            client.aliases.set(alias, pull.config.name)
-        });
-    });
-});
-
-client.on("message", async message => {
-    if(message.author.client || message.channel.type === "dm") return;
-
-    let prefix = clientsettings.prefix;
-    let messageArray = message.content.split(" ")
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-
-    if(!message.content.startsWith(prefix)) return;
-    let commandfile = client.commands.get(cmd.slice(prefix.length)) || client.commands.get(client.aliases.get(cmd.slice(prefix.length)))
-    if(commandfile) commandfile.run(client,message,args)
-
-})
