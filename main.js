a3d78fbd4d1561d0c9583a687fe2e73e335b63ed
Dawish Wishub <dawishwishub@gmail.com>
2020-07-09 12:58:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -46,12 +46,19 @@ client.on('message', async message => {
         .setTitle(`Commands`)
         .setColor('#f4aea4')
         .addFields(
-		{ name: '.hi', value: `I will say hi back`, inline: true },
+		{ name: '.hi', value: `I'll say hi back`, inline: true },
 		{ name: '.me', value: `I'll send you tag and user ID`, inline: false },
 		{ name: '.suggest <whatever suggestion>', value: `I'll mark your suggestion`, inline: false },
 		{ name: '.help', value: `Shows a list of my commands`, inline: false },
 	)
         .setFooter('Use the suggestions command in the #suggestions channel and everything else in this channel')
         let msgEmbed = await helpChannel.send(embedhelp);
-    } 
+    } else if (message.content.startsWith(`${prefix}poll`)) {
+        pollChannelID = message.channel.id;
+        let pollChannel = message.guild.channels.cache.find(channel => channel.id === pollChannelID);
+        let pollTitle = message.content.split(" ").slice(1).join(" ")
+
+        let embedpoll = new Discord.MessageEmbed()
+        .setTitle(pollTitle)
+    }
 });
