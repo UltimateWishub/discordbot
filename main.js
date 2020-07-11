76f3b7c63aafa9fbf1c4c9c23c46b0db4023b127
Dawish Wishub <dawishwishub@gmail.com>
2020-07-11 18:27:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const {prefix} = require('./config.json');
 
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+const client = ({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 client.on("ready", async () => {
     console.log('Dn.Bot is online!');
