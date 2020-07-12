2959d374b05289323b9f67e5bac9a17050dab8f0
Dawish Wishub <dawishwishub@gmail.com>
2020-07-12 00:17:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 const {prefix} = require('./config.json');
 
 client.on("ready", async () => {
